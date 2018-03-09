dc0e937044e5750e5505f5e18da8405deb18a3e9
DJ <jajay_jubjup@hotmail.com>
2018-03-09 13:28:59 +0700

    update realtime

--- app/src/main/java/masterung/androidthai/in/th/qrfirebase/fragment/QRcodeFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/qrfirebase/fragment/QRcodeFragment.java
@@ -1,12 +1,22 @@
 package masterung.androidthai.in.th.qrfirebase.fragment;
 
+import android.app.Service;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.zxing.Result;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import masterung.androidthai.in.th.qrfirebase.R;
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
@@ -15,7 +25,10 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
  * Created by User on 9/3/2561.
  */
 
-public class QRcodeFragment extends Fragment implements ZXingScannerView.ResultHandler{
+public class QRcodeFragment extends Fragment{
+
+  //  Explicit
+    private ZXingScannerView zXingScannerView;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -32,8 +45,38 @@ public class QRcodeFragment extends Fragment implements ZXingScannerView.ResultH
             @Override
             public void onClick(View view) {
 
+            zXingScannerView = new ZXingScannerView(getActivity());
+            getActivity().setContentView(zXingScannerView);
 
-      //          QR scan
+            zXingScannerView.startCamera();
+            zXingScannerView.setResultHandler(new ZXingScannerView.ResultHandler() {
+                @Override
+                public void handleResult(Result result) {
+                    String resultString = result.getText().toString();
+                    if (resultString.length()!=0){
+                        Log.d("9MarchV1", "Result ==> " + resultString);
+
+  //                      Add Result to Realtime Service Firebase
+                        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                        DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+                        Map<String, Object> stringObjectMap = new HashMap<>();
+                        stringObjectMap.put("NewsFeed", resultString);
+                        databaseReference.updateChildren(stringObjectMap);
+
+                        zXingScannerView.removeAllViews();
+                        zXingScannerView.stopCameraPreview();
+                        zXingScannerView.stopCamera();
+
+                        Intent intent = getActivity().getIntent();
+                        getActivity().finish();
+                        startActivity(intent);
+
+                    }
+
+                }
+            });
+
 
             }
         });
@@ -45,6 +88,8 @@ public class QRcodeFragment extends Fragment implements ZXingScannerView.ResultH
         View view = inflater.inflate(R.layout.fragment_qrcode, container, false);
         return view;
     }
+
+
 } // Main Class
 
 
